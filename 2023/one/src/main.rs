44264267aeeeeb5cd12683ebe6b93090227cf590
Peyton Schmidt <peytonsch@gmail.com>
2024-02-18 14:51:32 -0500

    This was gross

--- 2023/one/src/main.rs
+++ 2023/one/src/main.rs
@@ -1,31 +1,26 @@
+use itertools::Itertools;
+
+const NUMBERS: &[&str] = &[
+    "0", "zero", "1", "one", "2", "two", "3", "three", "4", "four", "5", "five", "6", "six", "7",
+    "seven", "8", "eight", "9", "nine",
+];
+
 fn main() {
-    let contents = include_str!("input.txt");
-    let parts = contents.split('\n');
-    let mut numbs: Vec<u8> = vec![];
-    for part in parts {
-        let mut first: String = "".to_string();
-        let mut last: String = "".to_string();
-        // println!("{}", part.trim());
-        for char in part.chars() {
-            if char.is_numeric(){
-                // let digit = char.to_string().parse().unwrap();
-                if first == "" {
-                    first = char.to_string();
-                    last =  char.to_string();
-                }
-                else {
-                    last =  char.to_string();
-                }
-            }
-        }
-        first.push_str(&last);
-        println!("{}", first);
-        let digit: u8 = first.parse().unwrap();
-        numbs.push(digit);
-    }
-    let mut total:u32 = 0;
-    for numb in numbs {
-        total = total + numb as u32;
-    }
-    println!("{}", total)
+    let contents = include_str!("input-2.txt");
+
+
+    let sum: usize = contents
+        .lines()
+        .map(|line| {
+            let (a, b) = NUMBERS
+                .iter()
+                .enumerate()
+                .flat_map(|(i, &n)| line.match_indices(n).map(move |(idx, _)| (idx, i / 2)))
+                .minmax()
+                .into_option()
+                .unwrap();
+            a.1 * 10 + b.1
+        })
+        .sum();
+    println!("{}", sum);
 }
